d4d7508e239c886971e9988a08ea5e6cb71a0803
Nathan Larue <46934145+Nathan-Larue@users.noreply.github.com>
2024-11-27 06:24:24 -0500

    Added base template, React Query, TS, Styled Components

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import * as S from "./styles";
+
+function App() {
+  return (
+    <S.App>
+      <S.AppHeader>
+        <S.AppLogo src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <S.AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </S.AppLink>
+      </S.AppHeader>
+    </S.App>
+  );
+}
+
+export default App;
